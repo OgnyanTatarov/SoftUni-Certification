e857dee10eada35d28d57e0ceb401c1f3ec79242
Ognyan Tatarov <tatarovognyan@gmail.com>
2024-02-04 10:58:15 +0000

    Adding the solutions for the Methods Lab as well as the solution for the Kamino Factory for Arrays More Exercises

---
+++ SoftUni-Fundamentals-Java/Lab-01.08.24/src/MethodsLab/SignInteger.java
@@ -0,0 +1,16 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class SignInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        getSign(number);
+    }
+    public static void getSign(int number){
+        if (number < 0) System.out.printf("The number %d is negative.",number);
+        else if (number > 0) System.out.printf("The number %d is positive.",number);
+        else System.out.printf("The number %d is zero.",number);
+    }
+}
